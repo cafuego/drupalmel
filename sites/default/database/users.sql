e45262c1ee94cf95b8646d0b26f1490fb4747b6a
Peter Lieverdink <cafuego@lists2.luv.asn.au>
2010-06-19 14:21:11 +1000

    Add users.sql

---
+++ sites/default/database/users.sql
@@ -0,0 +1,75 @@
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: drupalmel
+-- ------------------------------------------------------
+-- Server version	5.0.77-d8-ourdelta54-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `users` (
+  `uid` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(60) NOT NULL default '',
+  `pass` varchar(32) NOT NULL default '',
+  `mail` varchar(64) default '',
+  `mode` tinyint(4) NOT NULL default '0',
+  `sort` tinyint(4) default '0',
+  `threshold` tinyint(4) default '0',
+  `theme` varchar(255) NOT NULL default '',
+  `signature` varchar(255) NOT NULL default '',
+  `signature_format` smallint(6) NOT NULL default '0',
+  `created` int(11) NOT NULL default '0',
+  `access` int(11) NOT NULL default '0',
+  `login` int(11) NOT NULL default '0',
+  `status` tinyint(4) NOT NULL default '0',
+  `timezone` varchar(8) default NULL,
+  `language` varchar(12) NOT NULL default '',
+  `picture` varchar(255) NOT NULL default '',
+  `init` varchar(64) default '',
+  `data` longtext,
+  `timezone_name` varchar(50) NOT NULL default '',
+  PRIMARY KEY  (`uid`),
+  UNIQUE KEY `name` (`name`),
+  KEY `access` (`access`),
+  KEY `created` (`created`),
+  KEY `mail` (`mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES
+  (0,'','','',0,0,0,'','',0,0,0,0,0,NULL,'','','',NULL,''),
+  (1,'ADMIN-USER-NAME',MD5('ADMIN-USER-PASSWORD'),'ADMIN-USER-EMAIL',0,0,0,'','',0,1266321598,1276299604,1276299604,1,'36000','','','ADMIN-USER-EMAIL','a:0:{}','Australia/Melbourne');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-06-19  3:40:05
